6d2284de9a5e960b505887b1887ee96db9c07ab0
o04051217-droid <o04051217@gmail.com>
2026-05-30 14:44:41 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/-Project-Workflow-Management-System/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
